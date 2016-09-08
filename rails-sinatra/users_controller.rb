bd28df87a415bcadbc8d406dce60edf876c9f5b6
gyeon <gyeon19@gmail.com>
2016-09-08 15:55:04 -0400

    rails CRUD

--- rails-sinatra/users_controller.rb
+++ rails-sinatra/users_controller.rb
@@ -3,19 +3,45 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  # /users/new
+  # /users/new --> get
 
   # create
+  def create
+    @user = User.create(user_params)
+    # /users --> post
+  end
 
   # index
+  def index
+    @users = User.all
+    # /users --> get
+  end
 
   # show
+  def show
+    @user = User.find(params[:id])
+    # /users/:id --> show
+  end
 
   # edit
+  def edit
+    @user = User.find(params[:id])
+    # /users/:id/edit --> get
+  end
 
   # update
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    # /users/:id --> patch
+  end
 
   # destroy
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    # /users/:id --> delete
+  end
 
 end
 
